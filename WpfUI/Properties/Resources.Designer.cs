f3b4db70ffa3606a2fbbc338812e73d82a8056fa
Brian Fontana <brian@codefoxtrot.com>
2022-03-28 15:29:42 -0400

    Add DataAccessLibrary and rename main project to WpfUI

--- WpfUI/Properties/Resources.Designer.cs
+++ WpfUI/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ContactBook.Properties {
+namespace WpfUI.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace ContactBook.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ContactBook.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WpfUI.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
