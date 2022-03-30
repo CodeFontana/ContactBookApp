65919be5eef58b5cb7c673b42f5e50f22c82f33d
Brian Fontana <brian@codefoxtrot.com>
2022-03-30 01:08:55 -0400

    Add custom textbox to provide PreviewText

---
+++ WpfUI/Controls/TextboxWithPreview.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WpfUI.Controls;
+
+public class TextboxWithPreview : TextBox
+{
+    static TextboxWithPreview()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(TextboxWithPreview), new FrameworkPropertyMetadata(typeof(TextboxWithPreview)));
+    }
+
+    public static DependencyProperty TextPreviewProperty = DependencyProperty.Register(
+        "TextPreview", 
+        typeof(string), 
+        typeof(TextboxWithPreview));
+
+    public string TextPreview
+    {
+        get { return (string)GetValue(TextPreviewProperty); }
+        set { SetValue(TextPreviewProperty, value); }
+    }
+
+    public static readonly DependencyProperty HasTextProperty =
+                DependencyProperty.Register("HasText",
+                    typeof(bool),
+                    typeof(TextboxWithPreview),
+                    new FrameworkPropertyMetadata(false,
+                        FrameworkPropertyMetadataOptions.BindsTwoWayByDefault 
+                        | FrameworkPropertyMetadataOptions.AffectsRender));
+
+    public bool HasText
+    {
+        get
+        {
+            return (bool)GetValue(HasTextProperty);
+        }
+        set
+        {
+            SetValue(HasTextProperty, value);
+        }
+    }
+
+    protected override void OnTextInput(TextCompositionEventArgs e)
+    {
+        if (string.IsNullOrEmpty(e.Text) == false)
+        {
+            HasText = true;
+        }
+        else
+        {
+            HasText = false;
+        }
+
+        base.OnTextInput(e);    
+    }
+}
