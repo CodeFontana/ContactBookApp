871854fff67fc916369c489f4cc66cce0877232b
Brian Fontana <brian@codefoxtrot.com>
2026-04-20 00:33:49 -0400

    Remove OnModelCreating() -- not necessary

--- DataAccessLibrary/ContactDbContext.cs
+++ DataAccessLibrary/ContactDbContext.cs
@@ -1,6 +1,5 @@
 ﻿using DataAccessLibrary.Entities;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace DataAccessLibrary;
 
@@ -8,19 +7,10 @@ public class ContactDbContext : DbContext
 {
     public ContactDbContext(DbContextOptions<ContactDbContext> options) : base(options)
     {
-
     }
 
     public DbSet<Person> Contacts { get; set; }
     public DbSet<Address> Addresses { get; set; }
     public DbSet<Email> Emails { get; set; }
     public DbSet<Phone> Phones { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        EntityTypeBuilder<Person> person = modelBuilder.Entity<Person>();
-        person.HasMany(p => p.Addresses);
-        person.HasMany(p => p.EmailAddresses);
-        person.HasMany(p => p.PhoneNumbers);
-    }
 }
